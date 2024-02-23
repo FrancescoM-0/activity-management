02455c65cf941f572c09b5ec6fd50b2972724030
Molinari Francesco <fmolinari@codearchitects.com>
2024-02-23 15:52:46 +0100

    undo and redo added

--- src/components/task_manager/TaskManager.tsx
+++ src/components/task_manager/TaskManager.tsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import SearchBar from "../SearchBar";
 import TMUserList from "./TMUserList";
 import TMTaskList from "./TMTaskList";
+import { useAppDispatch } from "../../redux/hooks";
+import { redoTasks, undoTasks } from "../../redux/reducers/tasksSlice";
 
 function TaskManager() {
   const [userFilter, setUserFilter] = useState<string>("");
   const [textFilter, setTextFilter] = useState<string>("");
   const [statusFilter, setStatusFilter] = useState<string>("");
+  const dispatch = useAppDispatch();
 
   return (
     <>
@@ -16,7 +19,20 @@ function TaskManager() {
         onStatusFilterChange={setStatusFilter}
         onUserFilterChange={setUserFilter}
       ></SearchBar>
-
+      <button
+        onClick={() => {
+          dispatch(undoTasks());
+        }}
+      >
+        undo
+      </button>
+      <button
+        onClick={() => {
+          dispatch(redoTasks());
+        }}
+      >
+        redo
+      </button>
       <TMUserList onUserFilterChange={setUserFilter}></TMUserList>
       <TMTaskList
         userFilter={userFilter}
